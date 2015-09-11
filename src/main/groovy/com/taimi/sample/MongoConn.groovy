9ebd8d2f84cf5b218600ddbcb1f687be6565ca5c
Jyrki Oinas <jyrki.oinas@capgemini.com>
2015-09-11 13:28:50 +0200

    Added spring application context to load the configuration (db conn) from outside the code. NOTE! you need to add file sample-portlet.properties under CATALINA_HOME/conf containing properties: mongodb.host=localhost mongodb.port=27017

---
+++ src/main/groovy/com/taimi/sample/MongoConn.groovy
@@ -0,0 +1,65 @@
+package com.taimi.sample
+
+import java.util.List;
+import java.util.Set;
+import javax.annotation.Resource
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+import com.mongodb.ServerAddress;
+
+import org.springframework.stereotype.Component;
+import org.springframework.data.mongodb.MongoDbFactory
+
+import org.apache.log4j.Logger
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@Component
+class MongoConn {
+	Logger log = Logger.getLogger(MongoConn.class)
+	
+	@Resource(name="mongoDbFactory")
+	MongoDbFactory mongoFactory
+	
+	public MongoConn() {
+	// To directly connect to a single MongoDB server (note that this will not auto-discover the primary even
+	// if it's a member of a replica set:
+		log.info("MongoConn constructor")
+		log.info("End of MongoConn constructor")
+	}
+	
+	public String getRestaurantsAddress(String restaurant) {
+		log.info("getRestaurantsAddress ${restaurant}")
+		try {			
+			def db = mongoFactory.getDb();
+			DBCollection coll = db.getCollection("restaurants");
+			BasicDBObject query = new BasicDBObject("name", restaurant);
+			
+			DBCursor cursor = coll.find(query);
+			
+			if(cursor.hasNext()) {
+				log.info("found ${restaurant}")
+				def dbo = cursor.next()
+				log.info("dbo: ${dbo}")
+				StringBuilder sb = new StringBuilder();
+				sb.append(dbo.get("address"))
+				def result = sb.toString()
+				log.info("result: ${result}")
+				return result;
+			}
+			
+			return ""
+		} finally {
+			log.info("No need to close connection")
+		}
+	}
+	
+}
