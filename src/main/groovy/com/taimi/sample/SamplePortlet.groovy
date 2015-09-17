1cf06d122dfb348c1cf3bdb72b50773e9ca75106
ttikka <tommi.tikka@capgemini.com>
2015-09-17 15:19:10 +0300

    Sample portlet render call prints current user data to stdout

--- src/main/groovy/com/taimi/sample/SamplePortlet.groovy
+++ src/main/groovy/com/taimi/sample/SamplePortlet.groovy
@@ -4,6 +4,7 @@ import com.liferay.portal.kernel.exception.PortalException
 import com.liferay.portal.kernel.exception.SystemException
 import com.liferay.portal.kernel.util.ParamUtil
 import com.liferay.util.bridges.mvc.MVCPortlet
+import com.liferay.portal.util.PortalUtil
 
 import javax.portlet.ActionRequest
 import javax.portlet.ActionResponse
@@ -17,6 +18,7 @@ public class SamplePortlet extends MVCPortlet {
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
 		println '*** SamplePortlet RENDER ***'
+		println 'Current user: ' + PortalUtil.getUser(renderRequest)
 		super.render(renderRequest, renderResponse)
 	}
 	
